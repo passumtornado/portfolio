05bc957a036a6f2a04889b706a0d9344c5aa3491
passumtornado <passumtornado10@gmail.com>
2026-03-21 01:25:00 +0100

    created new files for the sections of the portfolio website, including About, Experience, Hero, Projects, and an index file to export them all. These components will be used to build the different sections of the portfolio website, allowing for a modular and organized structure. Each section will contain relevant information and content to showcase my skills, experience, and projects effectively.

---
+++ src/sections/index.ts
@@ -0,0 +1,13 @@
+import { Projects } from './Projects';
+import { Hero } from './Hero';
+import { Experience } from './Experience';
+import { Contact } from './Contact';
+import { About } from './About';
+
+export {
+  Hero,
+  Experience,
+  Contact,
+  About,
+  Projects
+}
